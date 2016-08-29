cd835b889c15624fb51639c20e36e78c334dd9e4
123960 <vinicius.apk@gmail.com>
2016-08-29 09:34:56 -0300

    t

--- toc/erlang/automata_lfc.erl
+++ toc/erlang/automata_lfc.erl
@@ -15,11 +15,10 @@ new_lfc(Q, E, D, Q0) -> #lfc{q=Q ++ [transition_not_found],
 
 state_after_w(L=#lfc{}, W) -> r_state_after_w(L, W, L#lfc.q0).
 
-r_state_after_w(L=#lfc{}, [Wh|_], Qn)  -> io:format("Wh=%s", Wh),
-                                          next_status(Qn, Wh, L#lfc.d);
-r_state_after_w(L=#lfc{}, [Wh|Wt], Qn) -> r_state_after_w(L, Wt, next_status(Qn, Wh, L#lfc.d)).
+r_state_after_w(L=#lfc{}, [Wh|Wt], Qn) -> r_state_after_w(L, Wt, next_status(Qn, Wh, L#lfc.d));
+r_state_after_w(L=#lfc{}, _, Qn)       -> Qn.
 
-next_status(Q0, E, D) -> [Qf || {En, Qi, Qf} <- D, En == E, Qi == Q0].
+next_status(Q0, E, D) -> hd([Qf || {En, Qi, Qf} <- D, En == E, Qi == Q0]).
 
 accept_w(#lfc{}, W) -> accept.
 
--- toc/erlang/automata_lfc_testes.erl
+++ toc/erlang/automata_lfc_testes.erl
@@ -16,10 +16,12 @@ ZAPUNLIMITED_lfc = automata_lfc:new_lfc([0,1], [5000, 5001], [{5000, 1, 0},
                                                               {5001, 1, 0},
                                                               {5001, 0, 1}], 0).
 
-automata_lfc:state_after_w(ZAPUNLIMITED_lfc, [5000, 5000, 5001, 5000, 5001]).
+automata_lfc:state_after_w(ZAPUNLIMITED_lfc, [5000, 5000, 5001, 5000]).
 
 automata_lfc:next_status(0, 5000, [{5000, 1, 0},
                                    {5000, 0, 0},
                                    %%{5001, 0, 0}, Não implementado as applicabilities ainda
                                    {5001, 1, 0},
                                    {5001, 0, 1}]).
+
+erlang:display(lists:flatten("Qn=" ++ io_lib:format("~p", [Qn]) ++ ",Wh=" ++ io_lib:format("~p", [Wh]))),
