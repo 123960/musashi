5f9c601a157c7d84529d037550e4adebca292623
123960 <vinicius.apk@gmail.com>
2016-08-31 09:40:24 -0300

    t

--- toc/erlang/automata_lfc.erl
+++ toc/erlang/automata_lfc.erl
@@ -1,5 +1,5 @@
 -module(automata_lfc).
--export([state_after_w/2, new_lfc/4, next_status/3]).
+-export([state_after_w/2, new_lfc/4, next_status/3, full_d/2, accept_w/2]).
 
 -record(lfc, {q, %%estados
               e, %%eventos
@@ -16,11 +16,21 @@ new_lfc(Q, E, D, Q0) -> #lfc{q=Q ++ [transition_not_found],
 state_after_w(L=#lfc{}, W) -> r_state_after_w(L, W, L#lfc.q0).
 
 r_state_after_w(L=#lfc{}, [Wh|Wt], Qn) -> r_state_after_w(L, Wt, next_status(Qn, Wh, L#lfc.d));
-r_state_after_w(L=#lfc{}, _, Qn)       -> Qn.
+r_state_after_w(#lfc{}, _, Qn)         -> Qn.
 
 next_status(Q0, E, D) -> hd([Qf || {En, Qi, Qf} <- D, En == E, Qi == Q0]).
 
-accept_w(#lfc{}, W) -> accept.
+accept_w(L=#lfc{}, W) -> case state_after_w(L, W) of
+                           transition_not_found -> false;
+                           Qf                   -> sets:is_element(Qf, sets:from_list(L#lfc.qf))
+                         end.
 
-%%Inclui transition_not_found nas lacunas de QxE para D
-define_d(Q, E, D) -> D.
+%%Inclui transition_not_found nas lacunas de QxE -> D
+define_d(Q, E, D) -> L  = sets:from_list([{Qi, Ei} || {Qi, Ei, _} <- D]),
+                     FD = sets:from_list(full_d(Q, E)),
+                     T  = [{X, Y, transition_not_found} || {X, Y} <- sets:to_list(sets:subtract(FD, L))],
+                     T2 = [{X, transition_not_found, transition_not_found} || X <- E],
+                     D ++ T ++ T2.
+
+%%Cria todas as transicoes de QxE
+full_d(Q, E) -> lists:flatmap(fun(X) -> lists:map(fun(Y) -> {X, Y} end, Q) end, E).
--- toc/erlang/automata_lfc_testes.erl
+++ toc/erlang/automata_lfc_testes.erl
@@ -12,16 +12,25 @@ rr(automata_lfc).
 %%SELECT distinct event_id, actual_status, next_status FROM NGIN_STS2_CFG_FLOW WHERE CFG_ID = 5012;
 ZAPUNLIMITED_lfc = automata_lfc:new_lfc([0,1], [5000, 5001], [{5000, 1, 0},
                                                               {5000, 0, 0},
-                                                              %%{5001, 0, 0}, Não implementado as applicabilities ainda
-                                                              {5001, 1, 0},
-                                                              {5001, 0, 1}], 0).
+                                                              {5001, 1, 0}], 0).
 
-automata_lfc:state_after_w(ZAPUNLIMITED_lfc, [5000, 5000, 5001, 5000]).
+automata_lfc:state_after_w(ZAPUNLIMITED_lfc, [5000, 5000, 5001]).
 
-automata_lfc:next_status(0, 5000, [{5000, 1, 0},
-                                   {5000, 0, 0},
-                                   %%{5001, 0, 0}, Não implementado as applicabilities ainda
-                                   {5001, 1, 0},
-                                   {5001, 0, 1}]).
+automata_lfc:next_status(0, 5001, [{5000,1,0},
+                                   {5000,0,0},
+                                   {5001,1,0},
+                                   {5001,0,transition_not_found}]).
 
 erlang:display(lists:flatten("Qn=" ++ io_lib:format("~p", [Qn]) ++ ",Wh=" ++ io_lib:format("~p", [Wh]))),
+
+automata_lfc:full_d([5000, 5001], [0,1]).
+
+[{5000, 1, 0},
+ {5000, 0, 0},
+ {5001, 1, 1}] ++
+[{X, Y, transition_not_found} || {X, Y} <- sets:to_list(sets:subtract(sets:from_list(automata_lfc:full_d([5000, 5001], [0,1])), sets:from_list([{5000, 1},
+                                                                                                                                                {5000, 0},
+                                                                                                                                                {5001, 1}])))]
+
+
+automata_lfc:accept_w(ZAPUNLIMITED_lfc, [5000, 5000, 5001]).
